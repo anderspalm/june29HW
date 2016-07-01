916fdec1d5ea2f70794da906baff105adfd11828
anderspalm <ang.palm@hotmail.com>
2016-06-30 23:46:24 -0400

    Update MainActivity.java

--- june29HW/views-101-lesson/Android-Views-Lab/app/src/main/java/com/example/generalassembly/android_views_lab/MainActivity.java
+++ june29HW/views-101-lesson/Android-Views-Lab/app/src/main/java/com/example/generalassembly/android_views_lab/MainActivity.java
@@ -40,54 +40,53 @@ public class MainActivity extends AppCompatActivity {
         array.add("seven");array.add("eight");array.add("nine");array.add("ten");
 
 
-        for(int i = 0; i<array.size(); i ++ ){
-            if(findViewById(array.indexOf(i)).equals(array.indexOf(i))) {
-                 int jam = array.indexOf(i);
+        // for(int i = 0; i<array.size(); i ++ ){
+        //     if(findViewById(array.indexOf(i)).equals(array.indexOf(i))) {
+        //          int jam = array.indexOf(i);
                 View.OnClickListener name = new View.OnClickListener() {
                     @Override
                     public void onClick(View fg) {
 //                for(int i = 0; i<array.size(); i ++ ){
 //                    if(findViewById(array.indexOf(i)).equals(array.indexOf(i))) {
-                        Toast.makeText(fg.getContext(), "You clicked on the button below views number " + jam * 2, Toast.LENGTH_LONG).show();
+                 //       Toast.makeText(fg.getContext(), "You clicked on the button below views number " + jam * 2, Toast.LENGTH_LONG).show();
 
 //                    }
 //                }
 //                mButtonOne.setText("Hello");
-//                if (findViewById(R.id.one) == view) {
-//                    Toast.makeText(view.getContext(), "You clicked on the button below views number 2", Toast.LENGTH_LONG).show();
-//                }
-//                if (findViewById(R.id.two) == view) {
-//                    Toast.makeText(view.getContext(), "You clicked on the button below views number 4", Toast.LENGTH_LONG).show();
-//                }
-//                if (findViewById(R.id.three) == view) {
-//                    Toast.makeText(view.getContext(), "You clicked on the button below views number 6", Toast.LENGTH_LONG).show();
-//                }
-//                if (findViewById(R.id.four) == view) {
-//                    Toast.makeText(view.getContext(), "You clicked on the button below views number 8", Toast.LENGTH_LONG).show();
-//                }
-//                if (findViewById(R.id.five) == view) {
-//                    Toast.makeText(view.getContext(), "You clicked on the button below views number 10", Toast.LENGTH_LONG).show();
-//                }
-//                if (findViewById(R.id.six) == view) {
-//                    Toast.makeText(view.getContext(), "You clicked on the button below views number 12", Toast.LENGTH_LONG).show();
-//                }
-//                if (findViewById(R.id.seven) == view) {
-//                    Toast.makeText(view.getContext(), "You clicked on the button below views number 14", Toast.LENGTH_LONG).show();
-//                }
-//                if (findViewById(R.id.eight) == view) {
-//                    Toast.makeText(view.getContext(), "You clicked on the button below views number 16", Toast.LENGTH_LONG).show();
-//                }
-//                if (findViewById(R.id.nine) == view) {
-//                    Toast.makeText(view.getContext(), "You clicked on the button below views number 18", Toast.LENGTH_LONG).show();
-//                }
-//                if (findViewById(R.id.ten) == view) {
-//                    Toast.makeText(view.getContext(), "You clicked on the button below views number 20", Toast.LENGTH_LONG).show();
-//                }
+                        if (findViewById(R.id.one) == view) {
+                            Toast.makeText(view.getContext(), "You clicked on the button below views number 2", Toast.LENGTH_LONG).show();
+                        }
+                        if (findViewById(R.id.two) == view) {
+                            Toast.makeText(view.getContext(), "You clicked on the button below views number 4", Toast.LENGTH_LONG).show();
+                        }
+                        if (findViewById(R.id.three) == view) {
+                            Toast.makeText(view.getContext(), "You clicked on the button below views number 6", Toast.LENGTH_LONG).show();
+                        }
+                        if (findViewById(R.id.four) == view) {
+                            Toast.makeText(view.getContext(), "You clicked on the button below views number 8", Toast.LENGTH_LONG).show();
+                        }
+                        if (findViewById(R.id.five) == view) {
+                            Toast.makeText(view.getContext(), "You clicked on the button below views number 10", Toast.LENGTH_LONG).show();
+                        }
+                        if (findViewById(R.id.six) == view) {
+                            Toast.makeText(view.getContext(), "You clicked on the button below views number 12", Toast.LENGTH_LONG).show();
+                        }
+                        if (findViewById(R.id.seven) == view) {
+                            Toast.makeText(view.getContext(), "You clicked on the button below views number 14", Toast.LENGTH_LONG).show();
+                        }
+                        if (findViewById(R.id.eight) == view) {
+                            Toast.makeText(view.getContext(), "You clicked on the button below views number 16", Toast.LENGTH_LONG).show();
+                        }
+                        if (findViewById(R.id.nine) == view) {
+                            Toast.makeText(view.getContext(), "You clicked on the button below views number 18", Toast.LENGTH_LONG).show();
+                        }
+                        if (findViewById(R.id.ten) == view) {
+                            Toast.makeText(view.getContext(), "You clicked on the button below views number 20", Toast.LENGTH_LONG).show();
+                        }
                     }
                 };
-            }
-            }
-
+            // }
+            // }
         mButtonOne.setOnClickListener(name);
         mButtonTwo.setOnClickListener(name);
     }
